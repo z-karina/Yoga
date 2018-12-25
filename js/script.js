e0c5aebc4bd2bd461f1d2232cb53268a5e9e3ebd
karina <# FFB6B1 # FFB6B1 # FFB6B1 rosaline1306@gmail.comrosaline1306@gmail.com>
2018-12-25 13:02:33 +0200

    add slider

--- js/script.js
+++ js/script.js
@@ -2,4 +2,22 @@ $(document).ready(function () {
 
     $('.slider__item').height($(window).height());
 
+    $('.slider .owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        dots: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
 });
